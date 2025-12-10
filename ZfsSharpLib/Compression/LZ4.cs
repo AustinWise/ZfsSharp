b4f6142c44cca8ffa848c21779ae03c053067895
Austin Wise <AustinWise@gmail.com>
2025-12-09 19:26:07 -0800

    Use LZ4 from a Nuget package.

--- ZfsSharpLib/Compression/LZ4.cs
+++ ZfsSharpLib/Compression/LZ4.cs
@@ -1,8 +1,5 @@
-﻿using LZ4ps;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
+using K4os.Compression.LZ4;
 
 namespace ZfsSharpLib
 {
@@ -13,7 +10,7 @@ namespace ZfsSharpLib
             var bufsiz = input[0] << 24 | input[1] << 16 | input[2] << 8 | input[3];
             if (bufsiz + 4 > input.Length || bufsiz < 0)
                 throw new ArgumentOutOfRangeException("Not enough bytes in input.");
-            int ret = LZ4Codec.Decode64(input, 4, bufsiz, output, 0, output.Length, true);
+            int ret = LZ4Codec.Decode(input.Slice(4, bufsiz), output);
             if (ret != output.Length)
                 throw new Exception("Did not decompress the right number of bytes.");
         }
