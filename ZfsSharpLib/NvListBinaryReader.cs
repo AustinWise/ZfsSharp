42dcfe2e35ed350dc0c984805575385f87e228a3
Austin Wise <AustinWise@gmail.com>
2026-02-08 10:27:50 -0800

    Don't use BinaryReady in NVList code
    Just read as the correct endianness the first time.

--- ZfsSharpLib/NvListBinaryReader.cs
+++ ZfsSharpLib/NvListBinaryReader.cs
@@ -1,84 +1,74 @@
-﻿using System.IO;
+﻿using System;
+using System.Buffers.Binary;
+using System.IO;
 using System.Text;
 
 namespace ZfsSharpLib
 {
-    class NvListBinaryReader : BinaryReader
+    class NvListBinaryReader
     {
+        readonly Stream mStream;
         readonly Encoding mEnc;
         public NvListBinaryReader(Stream s)
-            : base(s, Encoding.UTF8)
+            : this(s, Encoding.UTF8)
         {
         }
         public NvListBinaryReader(Stream s, Encoding enc)
-            : base(s, enc)
         {
+            mStream = s;
             mEnc = enc;
         }
 
-        public override short ReadInt16()
+        private ReadOnlySpan<byte> ReadInternal(Span<byte> buffer)
         {
-            byte b0 = ReadByte();
-            byte b1 = ReadByte();
-            return (short)((b0 << 8) | b1);
+            mStream.ReadExactly(buffer);
+            return buffer;
         }
 
-        public override int ReadInt32()
+        public byte ReadByte()
         {
-            byte b0 = ReadByte();
-            byte b1 = ReadByte();
-            byte b2 = ReadByte();
-            byte b3 = ReadByte();
-            return (b0 << 24) | (b1 << 16) | (b2 << 8) | b3;
+            int ret = mStream.ReadByte();
+            if (ret == -1)
+                throw new EndOfStreamException();
+            return (byte)ret;
         }
 
-        public override long ReadInt64()
+        public short ReadInt16()
         {
-            long b0 = ReadByte();
-            long b1 = ReadByte();
-            long b2 = ReadByte();
-            long b3 = ReadByte();
-            long b4 = ReadByte();
-            long b5 = ReadByte();
-            long b6 = ReadByte();
-            long b7 = ReadByte();
-            return (b0 << 56) | (b1 << 48) | (b2 << 40) | (b3 << 32) | (b4 << 24) | (b5 << 16) | (b6 << 8) | b7;
+            return BinaryPrimitives.ReadInt16BigEndian(ReadInternal(stackalloc byte[sizeof(short)]));
         }
 
-        public override ushort ReadUInt16()
+        public int ReadInt32()
         {
-            byte b0 = ReadByte();
-            byte b1 = ReadByte();
-            return (ushort)((b0 << 8) | b1);
+            return BinaryPrimitives.ReadInt32BigEndian(ReadInternal(stackalloc byte[sizeof(int)]));
         }
 
-        public override uint ReadUInt32()
+        public long ReadInt64()
         {
-            byte b0 = ReadByte();
-            byte b1 = ReadByte();
-            byte b2 = ReadByte();
-            byte b3 = ReadByte();
-            return (uint)((b0 << 24) | (b1 << 16) | (b2 << 8) | b3);
+            return BinaryPrimitives.ReadInt64BigEndian(ReadInternal(stackalloc byte[sizeof(long)]));
         }
 
-        public override ulong ReadUInt64()
+        public ushort ReadUInt16()
         {
-            ulong b0 = ReadByte();
-            ulong b1 = ReadByte();
-            ulong b2 = ReadByte();
-            ulong b3 = ReadByte();
-            ulong b4 = ReadByte();
-            ulong b5 = ReadByte();
-            ulong b6 = ReadByte();
-            ulong b7 = ReadByte();
-            return (b0 << 56) | (b1 << 48) | (b2 << 40) | (b3 << 32) | (b4 << 24) | (b5 << 16) | (b6 << 8) | b7;
+            return BinaryPrimitives.ReadUInt16BigEndian(ReadInternal(stackalloc byte[sizeof(ushort)]));
         }
 
-        public override string ReadString()
+        public uint ReadUInt32()
+        {
+            return BinaryPrimitives.ReadUInt32BigEndian(ReadInternal(stackalloc byte[sizeof(uint)]));
+        }
+
+        public ulong ReadUInt64()
+        {
+            return BinaryPrimitives.ReadUInt64BigEndian(ReadInternal(stackalloc byte[sizeof(ulong)]));
+        }
+
+        public string ReadString()
         {
             int size = ReadInt32();
-            byte[] bytes = ReadBytes((size + 3) & ~3);
-            return mEnc.GetString(bytes, 0, size);
+            Span<byte> buffer = stackalloc byte[checked(size + 3) & ~3];
+            mStream.ReadExactly(buffer);
+            return mEnc.GetString(buffer.Slice(0, size));
         }
     }
 }
